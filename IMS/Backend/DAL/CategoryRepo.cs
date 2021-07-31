40366ba9030bde241dfca8a70354fb0169e41768
AsefHossainKhan <56684655+AsefHossainKhan@users.noreply.github.com>
2021-07-31 19:38:50 +0600

    Base setup of the final assignment

---
+++ IMS/Backend/DAL/CategoryRepo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class CategoryRepo
+    {
+        static IMSEntities context;
+
+        static CategoryRepo()
+        {
+            context = new IMSEntities();
+        }
+
+        public static List<category> GetAllCategories()
+        {
+            return context.categories.ToList();
+        }
+    }
+}
