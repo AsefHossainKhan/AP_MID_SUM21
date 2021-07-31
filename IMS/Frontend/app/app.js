b0d36e9eb9b36cc3a7c7955fe9780489683f52ad
AsefHossainKhan <56684655+AsefHossainKhan@users.noreply.github.com>
2021-07-31 21:56:13 +0600

    Frontend base setup

---
+++ IMS/Frontend/app/app.js
@@ -0,0 +1,21 @@
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(["$routeProvider","$locationProvider",function($routeProvider,$locationProvider) {
+
+    $routeProvider
+    .when("/", {
+        templateUrl : "views/pages/homepage.html",
+        controller: "home"
+    })
+    .when("/products", {
+        templateUrl : "views/pages/products.html",
+        controller: 'products'
+    })
+    .when("/addproduct", {
+        templateUrl : "views/pages/addproduct.html",
+        controller: 'addproduct'
+    })
+    .otherwise({
+        redirectTo:"/"
+    });
+
+}]);
